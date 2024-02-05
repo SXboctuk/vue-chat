8821b3b5afc58e0170bcc80f11155cfc0a4611ee
Maxim <stargate.xboctuk@gmail.com@>
2024-02-05 20:52:17 +0300

    initialize project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style>
+#app {
+  font-family: sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/views/Welcome.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="welcome container">Welcome</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style>
+.welcome {
+  text-align: center;
+  padding: 20px 0;
+}
+</style>
