bd2af32e656726d090bee4519656f85b52a00672
Maxim <stargate.xboctuk@gmail.com@>
2024-02-07 21:37:28 +0300

    redirect after auth

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -2,14 +2,14 @@
   <div class="welcome container">
     Welcome
     <template v-if="showLogin">
-      <login-form />
+      <login-form @login="enterChat" />
       <p>
         No account yet? <span class="span-button" @click="showLogin = false"> Sign up </span>
         instead
       </p>
     </template>
     <template v-else>
-      <signup-form />
+      <signup-form @signup="enterChat" />
       <p>
         Already registered?
         <span class="span-button" @click="showLogin = true"> Log in </span>
@@ -23,8 +23,16 @@
 import SignupForm from '@/components/SignupForm.vue'
 import LoginForm from '@/components/LoginForm.vue'
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { routeNames } from '@/router'
 
 const showLogin = ref(false)
+
+const router = useRouter()
+
+const enterChat = () => {
+  router.push({ name: routeNames.Chatroom })
+}
 </script>
 
 <style lang="scss">
