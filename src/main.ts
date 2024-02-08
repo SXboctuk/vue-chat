2cdc3fc80e3ad0c00a8d6c701a8d27f6aedb26b5
Maxim <stargate.xboctuk@gmail.com@>
2024-02-08 15:51:37 +0300

    add initialize logic for root  App

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
-import { createApp } from 'vue'
+import { createApp, type ComponentPublicInstance } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { projectAuth } from './firebase/config'
 
 import '@/assets/main.css'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+let app: ComponentPublicInstance
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+projectAuth.authStateReady().then(() => {
+  return (app = createApp(App).use(router).mount('#app'))
+})
